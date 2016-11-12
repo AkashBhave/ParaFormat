f0a90bddb0a879e181ab46f89ed4a877092885cf
Akash Bhave <akash_bhave@hotmail.com>
2016-11-12 15:21:24 -0500

    Added initial base file. Contains nothing that will run inside.

---
+++ src/Researcher.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Created by akash on 11/10/2016.
+ */
+public class Researcher {
+
+    // Constructor
+    public Researcher() {
+
+    }
+
+    public static void main(String[] args) {
+        generateLink();
+    }
+
+    public static String generateLink() {
+        return "LINK GOES HERE";
+    }
+
+}
