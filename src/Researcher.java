9cd9927489685f6825d67d24dbaea81549bf42e8
Akash Bhave <akash_bhave@hotmail.com>
2016-11-13 08:32:28 -0500

    Added "googleSearch", updated "generateSearchTerm" "generateSearchTerm" now creates a query that has proper conversion of whitespaces. Added new method "googleSearch" which generates a URL that searches Google.

--- src/Researcher.java
+++ src/Researcher.java
@@ -1,3 +1,6 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
 import java.util.Scanner;
 
 /**
@@ -10,16 +13,14 @@ public class Researcher {
 
     }
 
-    public static void main(String[] args) {
-        System.out.println(generateSearchTerm());
+    public static void main(String[] args) throws MalformedURLException {
+        googleSearch(generateSearchTerm());
     }
 
-    /*
-    Creates a valid search term that can be used
-    in URLs
-     */
+    // Creates a valid search term that can be used in URLs
     private static String generateSearchTerm() {
         String searchTerm = "";
+        String finalSearchTerm = "";
         Scanner userInput = new Scanner(System.in);
 
         boolean inputCorrect = false;
@@ -27,13 +28,33 @@ public class Researcher {
             System.out.println("Enter a search term.");
 
             searchTerm = userInput.nextLine();
-            if (!searchTerm.isEmpty()) {
+            if (!searchTerm.isEmpty() && searchTerm.trim().length() > 0) {
                 inputCorrect = true;
             }
         }
 
-        return searchTerm;
+        if (searchTerm.contains(" ")) {
+            String[] fullSearchTerm = searchTerm.split(" ");
+            for (String s : fullSearchTerm) {
+                finalSearchTerm += s;
+                finalSearchTerm += "%20";
+            }
 
+        } else {
+            finalSearchTerm = searchTerm;
+        }
+
+        if (finalSearchTerm.contains("%20")) {
+            finalSearchTerm = finalSearchTerm.substring(0, finalSearchTerm.length() - 3);
+        }
+
+        return finalSearchTerm;
+
+    }
+
+    // Searches Google using the search term given
+    private static void googleSearch(String searchTerm) throws MalformedURLException {
+        URL googleUrl = new URL("https://www.google.com/#q=" + searchTerm);
     }
 
 }
