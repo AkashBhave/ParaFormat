f76fec589cc57c5f0fc73a13878f3e0ce976ffb7
Akash Bhave <akash_bhave@hotmail.com>
2016-11-13 21:04:35 -0500

    Added method "wikiTermSearch" This method will be used to generate a single Wikipedia URL that will then be parsed for information.

--- src/Researcher.java
+++ src/Researcher.java
@@ -21,7 +21,10 @@ public class Researcher {
     }
 
     public static void main(String[] args) throws IOException {
-        googleSearch(generateSearchTerm(), generateNumResults());
+        String searchTerm = generateSearchTerm();
+        int numResults = generateNumResults();
+        googleSearch(searchTerm, numResults);
+        wikiTermSearch(searchTerm, numResults);
     }
 
     // Creates a valid search term that can be used in URLs
@@ -76,7 +79,6 @@ public class Researcher {
                 inputCorrect = false;
             }
         }
-
         return finalNum;
     }
 
@@ -115,11 +117,25 @@ public class Researcher {
             linkBodys.add(bodyResult);
         }
 
-        // Prints the results of the query (for debug purposes)
-        for(int i = 0; i < linkHrefs.size(); i++) {
-            System.out.println(linkHrefs.get(i));
-            System.out.println(linkTitles.get(i));
-            System.out.println(linkBodys.get(i));
+        System.out.println("\n\n\n");
+    }
+
+    private static void wikiTermSearch(String searchTerm, int numResults) throws IOException {
+        String fullWikiURL = "http://en.wikipedia.org/w/index.php?search=" + searchTerm + "&title=Special:Search&fulltext=1";
+
+        // Start JSoup parsing
+        Document wikiTermsDocument = Jsoup.connect(fullWikiURL).userAgent("Mozilla/5.0").get();
+
+        Elements wikiTerms = wikiTermsDocument.select("div.mw-search-result-heading");
+
+        int counter = 0;
+
+        for (Element term : wikiTerms) {
+            if(counter < numResults) {
+                String wikiTerm = term.text();
+                System.out.println(wikiTerm);
+                counter++;
+            }
         }
     }
 
