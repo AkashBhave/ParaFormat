3d8250e0cb997460f909b4f4c57493f3bd8bb6db
Akash Bhave <akash_bhave@hotmail.com>
2016-11-13 20:44:05 -0500

    "googleSearch" parses google.com's webpage, returns link, title, body

--- src/Researcher.java
+++ src/Researcher.java
@@ -1,5 +1,12 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -13,8 +20,8 @@ public class Researcher {
 
     }
 
-    public static void main(String[] args) throws MalformedURLException {
-        googleSearch(generateSearchTerm());
+    public static void main(String[] args) throws IOException {
+        googleSearch(generateSearchTerm(), generateNumResults());
     }
 
     // Creates a valid search term that can be used in URLs
@@ -52,9 +59,68 @@ public class Researcher {
 
     }
 
+    private static int generateNumResults() {
+        Scanner userInput = new Scanner(System.in);
+        String userNum;
+        int finalNum = 0;
+
+        boolean inputCorrect = false;
+        while (!inputCorrect) {
+            System.out.println("Enter the number of results you want.");
+
+            userNum = userInput.nextLine();
+            try {
+                finalNum = Integer.parseInt(userNum);
+                inputCorrect = true;
+            } catch (NumberFormatException e) {
+                inputCorrect = false;
+            }
+        }
+
+        return finalNum;
+    }
+
     // Searches Google using the search term given
-    private static void googleSearch(String searchTerm) throws MalformedURLException {
-        URL googleUrl = new URL("https://www.google.com/#q=" + searchTerm);
+    private static void googleSearch(String searchTerm, int numResults) throws IOException {
+        // Array lists to hold the results of the parsing
+        ArrayList<String> linkHrefs = new ArrayList<String>();
+        ArrayList<String> linkTitles = new ArrayList<String>();
+        ArrayList<String> linkBodys = new ArrayList<String>();
+
+        String googleUrl = "https://www.google.com/search" + "?q=" + searchTerm + "&num=" + numResults;
+
+        // Start JSoup parsing
+        Document googleDocument = Jsoup.connect(googleUrl).userAgent("Mozilla/5.0").get();
+
+        // Elements to parse link title and URL
+        Elements titleResults = googleDocument.select("h3.r > a");
+        for (Element result : titleResults) {
+            String linkHref = result.attr("href");
+            linkHref = linkHref.substring(7, linkHref.indexOf("&"));
+            String linkText = result.text();
+
+            // Append results to ArrayLists
+            if (!linkHref.contains("?q=")) {
+                linkHrefs.add(linkHref);
+            }
+            linkTitles.add(linkText);
+
+        }
+
+        // Elements to parse the body
+        Elements bodyResults = googleDocument.select("span.st");
+        for (Element bResult : bodyResults) {
+            String bodyResult = bResult.text();
+
+            linkBodys.add(bodyResult);
+        }
+
+        // Prints the results of the query (for debug purposes)
+        for(int i = 0; i < linkHrefs.size(); i++) {
+            System.out.println(linkHrefs.get(i));
+            System.out.println(linkTitles.get(i));
+            System.out.println(linkBodys.get(i));
+        }
     }
 
 }
